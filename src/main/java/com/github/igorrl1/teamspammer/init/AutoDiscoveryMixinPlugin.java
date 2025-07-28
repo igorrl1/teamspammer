798c0465d9c56764b974d1f7871977a36403a0ee
GitHub Action <action@github.com>
2025-07-28 16:57:02 +0000

    Initialize template repository proper
    Set package name to com.github.igorrl1.teamspammer
    Set project name to teamspammer
    Set modid to teamspammer

--- src/main/java/com/github/igorrl1/teamspammer/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/igorrl1/teamspammer/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.igorrl1.teamspammer.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/igorrl1/teamspammer/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/igorrl1/teamspammer/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.igorrl1.teamspammer.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
